fadc7ab680cf7acb0e94b97402f8ed05cf7c4aff
Juan Lombana <manulom@gmail.com>
2016-06-16 00:26:44 -0500

    NSG

--- AdminAzureScripts/CreateVM.ps1
+++ AdminAzureScripts/CreateVM.ps1
@@ -1,19 +1,18 @@
-﻿#Add-AzureRmAccount
-#Login-AzureRmAccount
-#Get-AzureRmSubscription -SubscriptionName "ArgosID Bizspark" | Select-AzureRmSubscription
-#Set-AzureRmContext -SubscriptionName "ArgosID Bizspark"
-#Get-AzureRmContext
+﻿Add-AzureRmAccount
+Login-AzureRmAccount
+Get-AzureRmSubscription -SubscriptionName "ArgosID Bizspark" | Select-AzureRmSubscription
+Set-AzureRmContext -SubscriptionName "ArgosID Bizspark"
+Get-AzureRmContext
 
 #GRUPO DE RECURSOS
-#New-AzureRmResourceGroup -Name "RGTest1" -Location "Southcentral US"
-#$Resource = Get-AzureRmResourceGroup -Name "RGTest1"
-#$Resource.ResourceId
+New-AzureRmResourceGroup -Name "RGTest1" -Location "Southcentral US"
+$Resource = Get-AzureRmResourceGroup -Name "RGTest1"
+$Resource.ResourceId
 
 #CUENTA DE ALMACENAMIENTO
-#New-AzureRmStorageAccount -ResourceGroupName "RGTest1" -Name "satest170532" -SkuName Standard_LRS -Location "SouthCentral Us" -Kind Storage 
+New-AzureRmStorageAccount -ResourceGroupName "RGTest1" -Name "satest170532" -SkuName Standard_LRS -Location "SouthCentral Us" -Kind Storage 
 
 #CREAR MAQUINA VIRTUAL
-<#
 $Credentials = Get-Credential
 #CONFIGURACION DE LA VM
 $VMConfig = New-AzureRmVMConfig -VMName "VMTest1" -VMSize "Standard_A1"
@@ -32,58 +31,46 @@ $nic = New-AzureRmNetworkInterface -Force -Name "NICTest1" -ResourceGroupName "R
 $VMConfig = Add-AzureRmVMNetworkInterface -VM $VMConfig -Id $nic.Id
 #CREAR MAQUINA VIRTUAL
 New-AzureRmVM -ResourceGroupName "RGTest1" -Location "Southcentralus" -VM $VMConfig
-#>
+
 
 #ELIMINAR MAQUINA VIRTUAL ASM
-#Import-AzurePublishSettingsFile -PublishSettingsFile "C:\Azure\argosid.publishsettings"
-#Get-AzureService -ServiceName "DevTest19222"
-#$VM = Get-AzureVM -ServiceName "DevTest19222" -Name "DevTest1"
-#Remove-AzureVM -ServiceName "DevTest19222" -Name "DevTest1"
-#Remove-AzureService -ServiceName "DevTest19222"
-#Remove-AzureV
-
+Import-AzurePublishSettingsFile -PublishSettingsFile "C:\Azure\argosid.publishsettings"
+Get-AzureService -ServiceName "DevTest19222"
+$VM = Get-AzureVM -ServiceName "DevTest19222" -Name "DevTest1"
+Remove-AzureVM -ServiceName "DevTest19222" -Name "DevTest1"
+Remove-AzureService -ServiceName "DevTest19222"
+Remove-AzureV
 
 #CAMBIAR TAMAÑO DE LA MÁQUINA VIRTUAL
-<#
 $VM = Get-AzureRmVM -Name "VMTest1" -ResourceGroupName "RGTest1"
 $VM = New-AzureRmVMConfig -VMName "VMTest1" -VMSize "Standard_A3"
 $VM.Location = "Southcentral US"
 Update-AzureRmVM -ResourceGroupName "RGTest1" -VM $VM
-#>
-
 
 #AGREGAR VM A VNET EXISTENTE.
-
 #OBTENER CONFIGURACIONES PARA EL DISCO
-#Get-AzureRmStorageAccount
-#Get-AzureRmVMImagePublisher -Location "Southcentralus"
-#Get-AzureRmVMImageOffer -Location "Southcentralus" -PublisherName "MicrosoftWindowsServer"
-#Get-AzureRmVMImageSku -Location "southcentralus" -PublisherName "MicrosoftWindowsServer" -Offer "WindowsServer"
-
+Get-AzureRmStorageAccount
+Get-AzureRmVMImagePublisher -Location "Southcentralus"
+Get-AzureRmVMImageOffer -Location "Southcentralus" -PublisherName "MicrosoftWindowsServer"
+Get-AzureRmVMImageSku -Location "southcentralus" -PublisherName "MicrosoftWindowsServer" -Offer "WindowsServer"
 #OBTENER CONFIGURACIONES PARA LA RED
-<#
-#Get-AzureRmVirtualNetwork -ResourceGroupName "RGTest1" -Name "VNetTest1"
-
 $Credentials = Get-Credential
 $VMConfig2 = New-AzureRmVMConfig -VMName "VmTest2" -VMSize "Basic_A1"
 $VMConfig2 = Set-AzureRmVMOperatingSystem -VM $VMConfig2 -Windows -ComputerName "vmtest12" -Credential ($Credentials)
 $VMConfig2 = Set-AzureRmVMSourceImage -VM $VMConfig2 -Version "latest" -PublisherName "MicrosoftWindowsServer" -Offer "WindowsServer" -Skus "2012-R2-Datacenter"
 $VMConfig2 = Set-AzureRmVMOSDisk -VM $VMConfig2 -VhdUri "https://satest170532.blob.core.windows.net/vhds/vhddmtest2.vhd" -CreateOption fromImage -Name "chdtest2.vhd" -Caching ReadWrite
-
 #CONFIGURAR RED
 $Vnet = Get-AzureRmVirtualNetwork -ResourceGroupName "RGTest1" -Name "VNetTest1"
 $subnet = $Vnet.Subnets[0]
 $pip = New-AzureRmPublicIpAddress -ResourceGroupName "RGTest1" -Name "PIPVMTest2" -Location "southcentralus" -AllocationMethod Static -DomainNameLabel "vmrosquitastest2"
 $nic = New-AzureRmNetworkInterface -ResourceGroupName "RGTest1" -Name "NICVMTest2" -Location "southcentralus" -Subnet $subnet -PublicIpAddress $pip -Primary
-
 $VMConfig2 = Add-AzureRmVMNetworkInterface -VM $VMConfig2 -NetworkInterface @($nic)
 New-AzureRmVM -ResourceGroupName "RGTest1" -Location "southcentralus" -VM $VMConfig2
-#>
 
 #AGREGAR DISCO A LA VM
-#$VMConfig1 = Get-AzureRmVm -ResourceGroupName "RGTest1" -Name "VmTest1"
-#$VMConfig1 = Add-AzureRmVMDataDisk -VM $VMConfig1 -Name "DiscoDatos1" -VhdUri "https://satest170532.blob.core.windows.net/vhds/discodatosvm1.vhd" -Caching ReadWrite -DiskSizeInGB 128 -CreateOption empty
-#Update-AzureRmVm -ResourceGroupName "RGTest1" -VM $VMConfig1
+$VMConfig1 = Get-AzureRmVm -ResourceGroupName "RGTest1" -Name "VmTest1"
+$VMConfig1 = Add-AzureRmVMDataDisk -VM $VMConfig1 -Name "DiscoDatos1" -VhdUri "https://satest170532.blob.core.windows.net/vhds/discodatosvm1.vhd" -Caching ReadWrite -DiskSizeInGB 128 -CreateOption empty
+Update-AzureRmVm -ResourceGroupName "RGTest1" -VM $VMConfig1
 
 #AGREGAR NIC A MAQUINA VIRTUAL
 #CADA NIC DEBE ACCEDER A UN SUBNET DIFERENTE.
---
+++ AdminAzureScripts/NSG.ps1
@@ -0,0 +1,26 @@
+#SE TOMA LA SUBRED DESCRITA ABAJO PARA APLICAR A ALGUNAS SUBNETS UN NSG Y MODIFICAR OTRAS EXISTENTES.
+
+#CREAR RED VIRTUAL CON 3 SUBREDES.
+#$subnetAplicacion = New-AzureRmVirtualNetworkSubnetConfig -Name "aplicacion" -AddressPrefix "172.16.0.0/24"
+#$subnetServidores = New-AzureRmVirtualNetworkSubnetConfig -Name "servidores" -AddressPrefix "172.16.1.0/28"
+#$subnetDatos = New-AzureRmVirtualNetworkSubnetConfig -Name "datos" -AddressPrefix "172.16.1.16/28"
+#New-AzureRmVirtualNetwork -Name "VNetClickIn" -ResourceGroupName "RGVNTest1" -Location "Southcentralus" -AddressPrefix "172.16.0.0/12" -Subnet $subnetAplicacion,$subnetServidores,$subnetDatos
+
+#DEFINICIÓN DE REGLAS ACL DEL NSG
+$reglaHttpEntrada = New-AzureRmNetworkSecurityRuleConfig -Name "HttpEntradaPermitirServidores" -Description "Permitir entrada http a subred de servidores" -Protocol Tcp  -SourceAddressPrefix "Internet" -SourcePortRange "*" -DestinationAddressPrefix "*" -DestinationPortRange "80" -Access Allow -Priority "100" -Direction Inbound
+$reglaSqlEntrada = New-AzureRmNetworkSecurityRuleConfig -Name "SqlEntradaPermitirDatos" -Description "Permitir entrada sql a subred de datos" -Protocol Tcp  -SourceAddressPrefix "172.16.1.0/28" -SourcePortRange "*" -DestinationAddressPrefix "*" -DestinationPortRange "1433" -Access Allow -Priority "100" -Direction Inbound
+
+#CREACIÓN DEL NSG
+$nsgServidores = New-AzureRmNetworkSecurityGroup -Name "NSGServidores" -ResourceGroupName "RGVNTest1" -Location "southcentralus" -SecurityRules @($reglaHttpEntrada)
+$nsgDatos = New-AzureRmNetworkSecurityGroup -Name "NSGDatos" -ResourceGroupName "RGVNTest1" -Location "southcentralus" -SecurityRules @($reglaSqlEntrada)
+
+#ASOCIAR A SUBNET EXISTENTE
+$VNet = Get-AzureRmVirtualNetwork -ResourceGroupName "RGVNTest1" -Name "VNetClickIn"
+$subnetServidores = Get-AzureRmVirtualNetworkSubnetConfig -VirtualNetwork $VNet -Name "servidores"
+Set-AzureRmVirtualNetworkSubnetConfig -Name "servidores" -VirtualNetwork $VNet -AddressPrefix "172.16.1.0/28" -NetworkSecurityGroup $nsgServidores
+Set-AzureRmVirtualNetwork -VirtualNetwork $VNet #GUARDAR CAMBIOS
+
+#DEFINIR NSG EN LA CREACIÓN DE LA SUBNET
+$VNet = Get-AzureRmVirtualNetwork -Name "VNetClickIn" -ResourceGroupName "RGVNTest1"
+Add-AzureRmVirtualNetworkSubnetConfig -VirtualNetwork $VNet -Name "satelites" -AddressPrefix "192.168.0.0/24" -NetworkSecurityGroup $nsgServidores
+Set-AzureRmVirtualNetwork -VirtualNetwork $VNet
